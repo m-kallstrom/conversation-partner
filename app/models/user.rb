04cc2269750687b6198c80f9c2b8a25d2fb30e58
Dmitri Stefanisin <email.ds@protonmail.com>
2017-09-30 14:24:37 -0500

    keep fixing devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
 
   has_many :conversations
   has_many :trouble_words
   has_many :sentences, through: :conversations
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'project#landing'
 
   get '/conversations/main' => 'conversations#new', as: 'new_conservation'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # get '/logout' => 'sessions#destroy'
   get '/about' => 'project#about'
 
 
   resources :conversations, only: [:show, :create]
-  resources :users, only: [:show, :create, :new]
 
 end
