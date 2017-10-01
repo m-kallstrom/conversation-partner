2609557663ad135982abde00ddb05301e7b7e0a9
Marla Kallstrom <brandykou@gmail.com>
2017-10-01 15:12:56 -0500

    unit test for conversation model:

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:conversation) {Conversation.create(user_id: 1)}
+
+    it "belongs to a user" do
+      expect(conversation.user_id).to eq(1)
+    end
+
 end
