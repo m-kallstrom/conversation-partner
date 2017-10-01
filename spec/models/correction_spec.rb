5f3a21fdc218551bcfad39eda3cfa85a97d0bf23
Marla Kallstrom <brandykou@gmail.com>
2017-10-01 15:35:50 -0500

    tests for new word model

--- spec/models/correction_spec.rb
+++ spec/models/correction_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Correction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:conversation) {Conversation.create(user_id: 1)}
+  let(:sentence) {Sentence.create(content: "I goed to the store.", conversation: conversation)}
+  let(:correction) {Correction.create(sentence: sentence, corrected_sentence: "I went to the store.")}
+
+    it "has a corrected sentence" do
+      expect(correction.corrected_sentence).to eq("I went to the store.")
+    end
+
+    it "belongs to a sentence" do
+      expect(correction.sentence).to eq(sentence)
+    end
+
 end
--- spec/models/new_word_spec.rb
+++ spec/models/new_word_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe NewWord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:conversation) {Conversation.create(user_id: 1)}
+  let(:new_word) {NewWord.create(word: "Turkey", definition: "A large bird, often eaten for Thanksgiving dinner", conversation: conversation)}
+
+    it "has a chosen word" do
+      expect(new_word.word).to eq("Turkey")
+    end
+
+    it "has a definition" do
+      expect(new_word.definition).to eq("A large bird, often eaten for Thanksgiving dinner")
+    end
+
+    it "belongs to a conversation" do
+      expect(new_word.conversation).to eq(conversation)
+    end
 end
