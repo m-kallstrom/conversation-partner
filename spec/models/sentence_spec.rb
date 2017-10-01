e92f342ae00f6ca8f20c4bafbab5b04682e765f8
Marla Kallstrom <brandykou@gmail.com>
2017-10-01 15:38:41 -0500

    add sentence model tests

--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Sentence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:conversation) {Conversation.create(user_id: 1)}
+  let(:sentence) {Sentence.create(content: "I goed to the store", conversation: conversation)}
+
+  it "has content from the user" do
+    expect(sentence.content).to eq("I goed to the store")
+  end
+
+  it "belongs to a conversation" do
+    expect(sentence.conversation).to eq(conversation)
+  end
 end
