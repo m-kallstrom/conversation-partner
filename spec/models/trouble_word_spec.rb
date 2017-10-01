702c3cf99eb7d52074d040d993792c2060b320e9
Marla Kallstrom <brandykou@gmail.com>
2017-10-01 15:43:58 -0500

    add model tests for trouble words

--- spec/models/trouble_word_spec.rb
+++ spec/models/trouble_word_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe TroubleWord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:conversation) {Conversation.create(user_id: 1)}
+  let(:sentence) {Sentence.create(content: "I goed to the store", conversation: conversation)}
+  let(:correction) {Correction.create(corrected_sentence: "I went to the store", sentence: sentence)}
+  let(:trouble_word) {TroubleWord.create(corrected_word: "went", correction: correction)}
+
+  it "has the correct version of the word the user used wrong" do
+    expect(trouble_word.corrected_word).to eq("went")
+  end
+
+  it "belongs to a correction" do
+    expect(trouble_word.correction).to eq(correction)
+  end
+
 end
