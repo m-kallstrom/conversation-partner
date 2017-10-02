c3c3e2ea47019f2133d18846b0210be50fecba8c
Dmitri Stefanisin <email.ds@protonmail.com>
2017-10-02 12:34:36 -0500

    add styling to menu, add invisible div to close menu

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
     $('.nav-open').click(function() {
       // $('#overlay').fadeIn(120);
       document.getElementById("navbar").style.width = "70%";
+      document.getElementById("left-side-close").style.width = "30%";
+      document.getElementById("left-side-close").style.display = "block";
+
     })
 
     $('.nav-close').click(function() {
       // $('#overlay').fadeOut();
       document.getElementById("navbar").style.width = "0";
+      document.getElementById("left-side-close").style.width = "0";
+      document.getElementById("left-side-close").style.display = "none";
     })
 
 });
