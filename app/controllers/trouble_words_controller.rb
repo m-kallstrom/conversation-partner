e73cbadbdcd8f47321453ad2bfe289be1f534109
Dmitri Stefanisin <email.ds@protonmail.com>
2017-10-02 16:43:48 -0500

    add daily word view feature

--- app/controllers/trouble_words_controller.rb
+++ app/controllers/trouble_words_controller.rb
@@ -2,7 +2,7 @@ class TroubleWordsController < ApplicationController
   before_action :set_trouble_word, only: [:show, :edit, :update, :destroy]
 
   def daily_word
-    @word_and_definition
+    @word_and_definition = LanguageHelper.daily_word
   end
 
   # GET /trouble_words
