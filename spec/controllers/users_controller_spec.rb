e5a905a088e7a3f11befffee5a335e1b91902b7d
Dmitri Stefanisin <email.ds@protonmail.com>
2017-10-03 17:42:02 -0500

    working on users_controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+    let!(:user) { User.create!(username: "bob", email: "bob@bob.com", password: "123456") }
+
+   describe "GET #show" do
+     it "responds with status code 200" do
+       get :show, {params: {id: 1}}
+       expect(response).to have_http_status 200
+     end
+
+     it "renders the :show template" do
+       get :show, {params: {id: 1}}
+       expect(response).to render_template(:show)
+     end
+    end
+
+    describe "POST #create" do
+    it "responds with status code 302 for signing in user" do
+      post :create, {params:  {session: {email: "bob@bob.com", password: "123456"}}}
+      expect(response).to have_http_status 302
+    end
+
+    it "creates a new session for signing in user" do
+      post :create, {params:  {session: {email: "bob@bob.com", password: "123456"}}}
+      expect(session[:user_id]).to eq(user.id)
+    end
+  end
+
+     # {params:  {session: {email: "bob@bob.com", password: "123456"}}}
 
 end
