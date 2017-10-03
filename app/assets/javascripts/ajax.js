c0fdc442535a4c6feaf0979e78f816f977c25e59
Clint Thacker <couscous@gmail.com>
2017-10-03 10:56:54 -0500

    Clean up javascript

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,8 +1,6 @@
-  // $(document).on('turbolinks:load', function(){
-  //   sendInput();
-  //   $('.A').hide();
-  // });
 
+
+// window.scrollTo(0,document.body.scrollHeight);
 
   $(document).ready(function() {
     $(document).bind('keypress',pressed);
@@ -19,12 +17,10 @@
 
 
 sendButton = function() {
-
   $('#input-form').on('submit', function(event) {
       sendInput(event);
   });
-
-}
+};
 
 sendInput = function(event) {
   event.preventDefault();
@@ -32,12 +28,8 @@ sendInput = function(event) {
       var $form = $('#input-form');
       var userInput = $form.find('textarea').val();
       $form.find('textarea').val("");
-      console.log(userInput);
       var data = $form.serialize();
-      console.log(data);
-
       var output = "<li class='t-left'><span class='dialog'> you said: </span><br> " + userInput + "</li>"
-      console.log(output);
       $("#output-form").append(output);
 
 
@@ -48,7 +40,6 @@ sendInput = function(event) {
       });
 
     $request.done(function(response) {
-      console.log(response);
       $("#output-form").append(response);
 
     })
