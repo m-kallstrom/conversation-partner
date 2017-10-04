d2612f48c6000d1fed0893b2c8e26821633d956b
Marla Kallstrom <brandykou@gmail.com>
2017-10-04 09:27:25 -0500

    add extra content model for tongue twisters:

---
+++ app/models/extra_content.rb
@@ -0,0 +1,13 @@
+class ExtraContent
+
+
+  def self.tongue_twister
+    tt= ["Peter Piper picked a peck of pickled peppers.", "She sells seashells by the seashore.", "How much wood would a woodchuck chuck", "I saw Susie sitting in a shoe shine shop.", "I scream, you scream, we all scream for ice cream!", ]
+
+  end
+
+
+  def self.
+
+
+end
